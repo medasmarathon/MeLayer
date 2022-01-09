d312d0ae8454f38063c9bc9e795e831faf63e546
Duc Luong <vinhduc91@outlook.com>
2022-01-09 12:22:17 +0700

    refactor

---
+++ medLayer/core/datatype/__init__.py
@@ -0,0 +1,2 @@
+from .probability import Probability
+from .influencedirection import InfluenceDirection
---
+++ medLayer/core/datatype/influencedirection.py
@@ -0,0 +1,7 @@
+class InfluenceDirection:
+  fro: int
+  to: int
+
+  def __init__(self, from_value, to_value):
+    self.fro = from_value
+    self.to = to_value
