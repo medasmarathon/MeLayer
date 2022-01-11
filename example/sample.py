3c7c21db48625c78ce341a42cf9e372b649c552e
Luong Duc <vinhduc91@outlook.com>
2022-01-11 22:04:50 +0700

    clean

--- example/sample.py
+++ example/sample.py
@@ -1,5 +1,4 @@
 from medLayer.core.datatype import Probability
 from medLayer.base import Node
-from medLayer import config
 
 x = Node()
--- medLayer/base/node.py
+++ medLayer/base/node.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass
 from ..interface import INode
-from .. import config
 
 
 @dataclass
