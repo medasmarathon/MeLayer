8cfdf72cae022fd42a18be21cbbb982f677f9273
Duc Luong <vinhduc91@outlook.com>
2022-01-14 11:30:36 +0700

    add test data

--- medLayer/tests/logic/conftest.py
+++ medLayer/tests/logic/conftest.py
@@ -5,31 +5,31 @@ from medLayer.base.event import Event
 from medLayer.base.host import Host
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def human_host():
   return Host("Human")
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def symptoms_layer():
   return ConceptLayer('Symptoms', 1)
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def disease_layer():
   return ConceptLayer('Disease', 2)
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def symptom_events():
-  return [Event('Fever'), Event('Pain')]
+  return [Event('Fever'), Event('Abdominal Pain'), Event('Coughing')]
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def disease_events():
-  return [Event('FUO')]
+  return [Event('Appendicitis'), Event("Flu")]
 
 
-@pytest.fixture
+@pytest.fixture(autouse=True)
 def impossible_event():
   return Event('Impossible')
