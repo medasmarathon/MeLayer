2c03dd366041d3219c7b7227eafae6494709aa60
Duc Luong <vinhduc91@outlook.com>
2022-01-14 10:41:11 +0700

    add config, change fixture names called in test

--- medLayer/tests/logic/calculations_test.py
+++ medLayer/tests/logic/calculations_test.py
@@ -2,7 +2,8 @@ import pytest
 
 
 def test_calculate_event_probability(
-    sample_host, sample_conceptlayers, sample_events, sample_impossible_event
+    human_host, conceptlayer_1, conceptlayer_2, events_for_layer_1, events_for_layer_2,
+    sample_impossible_event
     ):
   print(sample_impossible_event.name)
-  assert sample_impossible_event.name == "a"
+  assert sample_impossible_event.name == "Impossible"
