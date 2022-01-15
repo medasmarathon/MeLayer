1533bb5eeb71e12265d996c52f29c6768af994e2
Duc Luong <vinhduc91@outlook.com>
2022-01-15 10:34:38 +0700

    add logic + test cases

--- medLayer/core/interface/ilayer.py
+++ medLayer/core/interface/ilayer.py
@@ -6,3 +6,12 @@ class ILayer:
   name: str
   order: int
   nodes: List[INode]
+
+  def __key(self):
+    return (self.name)
+
+  def __hash__(self):
+    return hash(self.__key())
+
+  def __eq__(self, other) -> bool:
+    raise NotImplementedError
--- medLayer/tests/logic/test_calculations.py
+++ medLayer/tests/logic/test_calculations.py
@@ -4,7 +4,7 @@ import pytest
 from medLayer.base.conceptlayer import ConceptLayer
 from medLayer.base.event import Event
 from medLayer.base.host import Host
-from medLayer.base.logic.calculations import calculate_event_probability, event_observation_status, find_layer_of_event
+from medLayer.base.logic.calculations import calculate_event_probability, event_observation_status, find_layer_of_event, find_relations_targeting_event
 from medLayer.base.observations import Observation
 
 
@@ -33,7 +33,29 @@ def test_find_layer_of_event(
   assert find_layer_of_event(human_host, impossible_event) == None
 
 
+def test_find_relations_targeting_event(
+    init_dataset_with_relations, human_host: Host, symptom_events: List[Event],
+    disease_events: List[Event]
+    ):
+  assert len(find_relations_targeting_event(disease_events[0], human_host)) == 1
+  assert len(find_relations_targeting_event(symptom_events[0], human_host)) == 0
+
+
 def test_calculate_event_probability__should_return_0(
     init_dataset, human_host: Host, impossible_event: Event
     ):
   assert calculate_event_probability(impossible_event, human_host, []) == 0
+  assert calculate_event_probability(
+      impossible_event, human_host, [Observation(impossible_event, True)]
+      ) == 0
+
+
+def test_calculate_event_probability__should_return_1(
+    init_dataset, human_host: Host, symptom_events: List[Event], disease_events: List[Event]
+    ):
+  assert calculate_event_probability(
+      symptom_events[0], human_host, [Observation(symptom_events[0], True)]
+      ) == 1
+  assert calculate_event_probability(
+      disease_events[0], human_host, [Observation(disease_events[0], True)]
+      ) == 1
