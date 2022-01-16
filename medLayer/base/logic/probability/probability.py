50e5e6b4ff43e0b0d071f9629b1b11c7818a0247
Duc Luong <vinhduc91@outlook.com>
2022-01-16 17:44:56 +0700

    create class, but fail to implement logic

--- medLayer/base/logic/probability/probability.py
+++ medLayer/base/logic/probability/probability.py
@@ -5,30 +5,66 @@ from medLayer.base.observations import Observation
 from medLayer.core.datatype.probabilityvalue import ProbabilityValue
 
 
-def P(event: Event) -> ProbabilityValue:
-  return event.independent_prob
+class IProbExpression:
+  computed_event: Event
+
+  def compute(self):
+    raise NotImplementedError
 
 
-def P_pure(event: Event, observations: List[Observation]) -> ProbabilityValue:
-  # return independent prob if not observed
-  if event.independent_prob == 0 or event.independent_prob == 1:     # if prob is certain (not/ do occur) -> return status
-    return event.independent_prob
-  # else check if event is observed
-  for ob in observations:
-    if ob.node == event and ob.is_observed_occuring is not None:
-      return ProbabilityValue(ob.is_observed_occuring)
-  return event.independent_prob
+class IProbability:
+  value: ProbabilityValue
 
 
-def P_with_coexisting_relations(
-    event: Event, observations: List[Observation], coexisting_relations: List[CoexistingRelation]
-    ) -> ProbabilityValue:
-  # if event is observed => return status
-  if P_pure(event, observations) != event.independent_prob:
-    return P_pure(event, observations)
-  # else
-  for ob in observations:
-    for rel in coexisting_relations:
-      if ob.node == rel.to_node or ob.node == rel.fro_node:
-        print("Has relation --> do calculations")
-  return P(event)
+class CompoundEventNaming:
+
+  @classmethod
+  def AND(cls, event_A: Event, event_B: Event) -> str:
+    return f"{event_A.name} AND {event_B.name}"
+
+  @classmethod
+  def OR(self, event_A: Event, event_B: Event) -> str:
+    return f"{event_A.name} OR {event_B.name}"
+
+  @classmethod
+  def NOT(self, event_A: Event) -> str:
+    return f"NOT {event_A.name}"
+
+
+class Probability(IProbability):
+
+  @classmethod
+  def from_probability_expression(cls, prob_expression: IProbExpression):
+    return prob_expression.compute()
+
+  def __init__(self, event: Event):
+    self.value = event.independent_prob
+
+
+class ProbabilityExpression(IProbExpression):
+  event_list: List[Event]
+  numerator: IProbExpression
+  denominator: IProbExpression
+
+  def provide(self, event_list: List[Event]) -> IProbExpression:
+    self.event_list = event_list
+    return self
+
+  def exist(self, event: Event) -> IProbExpression:
+    self.computed_event = event
+    return self
+
+  def not_exist(self, event: Event) -> IProbExpression:
+    self.computed_event = Event(f"Not {event.name}").with_base_prob(1 - event.independent_prob)
+    return self
+
+  def both(self, event_A: Event, event_B: Event) -> IProbExpression:
+    for eve in self.event_list:
+      if eve.name == CompoundEventNaming.AND(event_A, event_B):
+        self.computed_event = eve
+        return self
+
+    self.computed_event = Event(CompoundEventNaming.AND(event_A, event_B)).with_base_prob(
+        event_A.independent_prob * event_B.independent_prob
+        )
+    return self
